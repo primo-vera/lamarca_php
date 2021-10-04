0d1a2a9836f0790789f8a9c38b9c63d93f1bc204
Keith Barreras <keith.barreras@gmail.com>
2021-10-03 20:25:50 -0400

    Separated header and footer, to finish side nav

---
+++ footer.php
@@ -0,0 +1,8 @@
+    </div>
+ </div>
+    <!-- js Scripts -->
+
+    <script src="./sideNav.js"></script>
+    <script src="./navTop.js"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>LaMarca Detailing Services</title>
+    <link rel="icon" type="image/x-icon" href="./lamarca.ico" />
+    <link rel="stylesheet" href="./aReset.css" />
+    <link rel="stylesheet" href="./style.css" />
+    <script src="https://kit.fontawesome.com/5017c7b0a5.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300&display=swap" rel="stylesheet" />
+  </head>
+</html>
+<body>
+  <div class="bodyWrap">
